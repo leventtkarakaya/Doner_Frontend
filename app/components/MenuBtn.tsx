f05a1e3b63b36f03f81facf59d57fa9606b96094
leventtkarakaya <leventtkarakaya@gmail.com>
2024-10-03 22:57:09 +0300

    Content media query

--- app/components/MenuBtn.tsx
+++ app/components/MenuBtn.tsx
@@ -1,9 +1,24 @@
-import React from "react";
-import { AlignJustify } from "lucide-react";
+"use client";
+import React, { useState } from "react";
+import MenuImage from "@/public/images/navbar/align-justify (1).png";
+import Image from "next/image";
+import style from "./MenuBtn.module.css";
 export default function MenuBtn() {
+  const [open, setOpen] = useState(false);
+  const handleMenuBoolan = () => {
+    setOpen((prev) => !prev);
+    console.log(open);
+  };
   return (
     <>
-      <AlignJustify size={25} />
+      <Image
+        src={MenuImage}
+        width={25}
+        height={25}
+        onClick={() => handleMenuBoolan()}
+        style={{ cursor: "pointer" }}
+        alt="Image"
+      />
     </>
   );
 }
